220ffb4dabc3175a8b9ed5659cf8fd3e9a3b62bf
Adrian Paul <adipaul1981@yahoo.ca>
2019-10-02 08:49:05 -0400

    Refactor ES member document

--- tests/test_service.py
+++ tests/test_service.py
@@ -12,12 +12,12 @@ def test_handler():
         "Records": [
             {
                 "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
-                "body": json.dumps({"id": "abc", "firstName": "John", "lastName": "Doe"})
+                "body": json.dumps({"_id": "abc", "firstName": "John", "lastName": "Doe"})
             },
             {
                 "messageId": "2e1424d4-f796-459a-8184-9c92662be6da",
                 "body": json.dumps(
-                    {"id": "def", "firstName": "Jane", "lastName": "River", "interests": ["Volley Ball", "Natation"],
+                    {"_id": "def", "firstName": "Jane", "lastName": "River", "isPublic": True, "interests": ["Volley Ball", "Natation"],
                      "virtualStudies": [{"_id": "1", "name": "Virtual Study 1"},
                                         {"_id": "12", "name": "Virtual Study 2"}]}),
             }
