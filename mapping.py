8bc29867a749242bfc8f6f03535517736caea5f8
Celine Pelletier <cpelletier@ferlab.bio>
2021-11-23 17:38:48 -0500

    SKFP-316: Update mapping to make it compatible with ES7

--- mapping.py
+++ mapping.py
@@ -27,145 +27,143 @@ MEMBER_INDEX = {
         }
     },
     "mappings": {
-        "member": {
-            "properties": {
-                "email": {
-                    "type": "keyword"
-                },
-                "hashedEmail": {
-                    "type": "keyword",
-                    "index": False
-                },
-                "institutionalEmail": {
-                    "type": "keyword"
-                },
-                "acceptedTerms": {
-                    "type": "boolean"
-                },
-                "isPublic": {
-                    "type": "boolean"
-                },
-                "isActive": {
-                    "type": "boolean"
-                },
-                "roles": {
-                    "type": "keyword"
-                },
-                "title": {
-                    "type": "keyword",
-                    "index": False
-                },
-                "firstName": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "fields": {
-                        "raw": {
-                            "type": "keyword"
-                        }
+        "properties": {
+            "email": {
+                "type": "keyword"
+            },
+            "hashedEmail": {
+                "type": "keyword",
+                "index": False
+            },
+            "institutionalEmail": {
+                "type": "keyword"
+            },
+            "acceptedTerms": {
+                "type": "boolean"
+            },
+            "isPublic": {
+                "type": "boolean"
+            },
+            "isActive": {
+                "type": "boolean"
+            },
+            "roles": {
+                "type": "keyword"
+            },
+            "title": {
+                "type": "keyword",
+                "index": False
+            },
+            "firstName": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "fields": {
+                    "raw": {
+                        "type": "keyword"
                     }
-                },
-                "lastName": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "fields": {
-                        "raw": {
-                            "type": "keyword"
-                        }
+                }
+            },
+            "lastName": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "fields": {
+                    "raw": {
+                        "type": "keyword"
                     }
-                },
-                "jobTitle": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "fields": {
-                        "raw": {
-                            "type": "keyword"
-                        }
+                }
+            },
+            "jobTitle": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "fields": {
+                    "raw": {
+                        "type": "keyword"
                     }
-                },
-                "institution": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "fields": {
-                        "raw": {
-                            "type": "keyword"
-                        }
+                }
+            },
+            "institution": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "fields": {
+                    "raw": {
+                        "type": "keyword"
                     }
-                },
-                "city": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "fields": {
-                        "raw": {
-                            "type": "keyword"
-                        }
+                }
+            },
+            "city": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "fields": {
+                    "raw": {
+                        "type": "keyword"
                     }
-                },
-                "state": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "fields": {
-                        "raw": {
-                            "type": "keyword"
-                        }
+                }
+            },
+            "state": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "fields": {
+                    "raw": {
+                        "type": "keyword"
                     }
-                },
-                "country": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "fields": {
-                        "raw": {
-                            "type": "keyword"
-                        }
+                }
+            },
+            "country": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "fields": {
+                    "raw": {
+                        "type": "keyword"
                     }
-                },
-                "eraCommonsID": {
-                    "type": "keyword"
-                },
-                "bio": {
-                    "type": "text",
-                    "analyzer": "autocomplete"
-                },
-                "story": {
-                    "type": "text",
-                    "analyzer": "autocomplete"
-                },
-                "interests": {
-                    "type": "text",
-                    "analyzer": "autocomplete",
-                    "fields": {
-                        "raw": {
-                            "type": "keyword"
-                        }
+                }
+            },
+            "eraCommonsID": {
+                "type": "keyword"
+            },
+            "bio": {
+                "type": "text",
+                "analyzer": "autocomplete"
+            },
+            "story": {
+                "type": "text",
+                "analyzer": "autocomplete"
+            },
+            "interests": {
+                "type": "text",
+                "analyzer": "autocomplete",
+                "fields": {
+                    "raw": {
+                        "type": "keyword"
                     }
-                },
-                "virtualStudies": {
-                    "properties": {
-                        "id": {
-                            "type": "keyword",
-                            "index": False
-                        },
-                        "name": {
-                            "type": "text",
-                            "analyzer": "autocomplete",
-                            "fields": {
-                                "raw": {
-                                    "type": "keyword"
-                                }
+                }
+            },
+            "virtualStudies": {
+                "properties": {
+                    "id": {
+                        "type": "keyword",
+                        "index": False
+                    },
+                    "name": {
+                        "type": "text",
+                        "analyzer": "autocomplete",
+                        "fields": {
+                            "raw": {
+                                "type": "keyword"
                             }
                         }
-                    },
-                    "type": "nested"
+                    }
                 },
-                "searchableInterests": {
-                    "type": "nested",
-                    "properties": {
-                        "name": {
-                            "type": "text",
-                            "analyzer": "autocomplete",
-                            "fields": {
-                                "raw": {
-                                    "type": "keyword"
-                                }
+                "type": "nested"
+            },
+            "searchableInterests": {
+                "type": "nested",
+                "properties": {
+                    "name": {
+                        "type": "text",
+                        "analyzer": "autocomplete",
+                        "fields": {
+                            "raw": {
+                                "type": "keyword"
                             }
                         }
                     }
